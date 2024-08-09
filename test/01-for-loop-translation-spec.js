4549320db89dcfc838ae42b4336c925ce918db73
daniel watson <ozzloy@each.do>
2024-08-08 20:13:08 -0700

    use AST, not RE, to determine if 'for' or 'while' exists
    the prior test incorrectly failed a student's code because the old
    loop was commented out with a multiline comment instead of a bunch of
    single line comments.

--- test/01-for-loop-translation-spec.js
+++ test/01-for-loop-translation-spec.js
@@ -2,6 +2,8 @@ const chai = require("chai");
 chai.use(require("chai-spies"));
 const expect = chai.expect;
 const aCounter = require("../problems/01-for-loop-translation");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
 
 describe("aCounter", function() {
   it('aCounter("apple") evaluates to 1', function () {
@@ -14,10 +16,20 @@ describe("aCounter", function() {
     expect(aCounter("aAapple")).to.eq(3);
   });
   it('should use a for loop instead of a while loop', function () {
-    const forRe = /\n\s*for.*{/;
-    const whileRe = /\n\s*while.*{/;
-    const func = aCounter.toString();
-    expect(func, "aCounter must use a for loop").to.match(forRe);
-    expect(func, "aCounter should not use a while loop").to.not.match(whileRe);
+    const aCounterString = aCounter.toString();
+    const aCounterAST = parser.parse(aCounterString);
+
+    let hasWhileLoop = false;
+    let hasForLoop = false;
+
+    traverse(aCounterAST, {
+      While() { hasWhileLoop = true; },
+      For() { hasForLoop = true; }
+    });
+
+    expect(hasForLoop,
+           "aCounter must use a for loop").to.be.true;
+    expect(hasWhileLoop,
+           "aCounter should not use a while loop").to.be.false;
   });
 });
--- test/02-while-loop-translation-spec.js
+++ test/02-while-loop-translation-spec.js
@@ -2,8 +2,10 @@ const chai = require("chai");
 chai.use(require("chai-spies"));
 const expect = chai.expect;
 const eCounter = require("../problems/02-while-loop-translation");
+const parser = require("@babel/parser");
+const traverse = require("@babel/traverse").default;
 
-describe("eCounter", function() {
+describe("eCounter", function () {
   it('eCounter("apple") evaluates to 1', function () {
     expect(eCounter("apple")).to.eq(1);
   });
@@ -14,10 +16,20 @@ describe("eCounter", function() {
     expect(eCounter("AppleEe")).to.eq(3);
   });
   it('should use a for loop instead of a while loop', function () {
-    const forRe = /\n\s*for.*{/;
-    const whileRe = /\n\s*while.*{/;
-    const func = eCounter.toString();
-    expect(func, "eCounter must use a while loop").to.match(whileRe);
-    expect(func, "eCounter should not use a for loop").to.not.match(forRe);
+    const eCounterString = eCounter.toString();
+    const eCounterAST = parser.parse(eCounterString);
+
+    let hasWhileLoop = false;
+    let hasForLoop = false;
+
+    traverse(eCounterAST, {
+      While() { hasWhileLoop = true; },
+      For() { hasForLoop = true; }
+    });
+
+    expect(hasWhileLoop,
+           "eCounter must use a while loop").to.be.true;
+    expect(hasForLoop,
+           "eCounter should not use a for loop").to.be.false;
   });
 });
